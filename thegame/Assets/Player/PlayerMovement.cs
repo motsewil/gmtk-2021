bf5d6348c8f40b114b6d8c0f7352b741e88b02a8
Darren Hobern <darren.hobern@gmail.com>
2021-06-12 14:39:50 +1200

    Fix groundedness

--- thegame/Assets/Player/PlayerMovement.cs
+++ thegame/Assets/Player/PlayerMovement.cs
@@ -18,8 +18,13 @@ public class PlayerMovement : MonoBehaviour {
 		private float moveAcceleration = 2f;
 	[BoxGroup("Movement")][Range(1, 10)][SerializeField]
 		private float moveSpeed = 10f;
+	[BoxGroup("Movement")][SerializeField]
+		private Vector2 rayOrigin = Vector2.zero;
+	[BoxGroup("Movement")][Range(0, 2)][SerializeField]
+		private float rayDist = 1f;
+	
 	private Vector2 moveVector;
-	private bool grounded;
+	private bool isGrounded;
 
 	[BoxGroup("Aiming")][SerializeField]
 		private Transform aimReticle;
@@ -49,7 +54,7 @@ public class PlayerMovement : MonoBehaviour {
 
 #region Movement
 	private void UpdateMovement() {
-		rigidbody.AddForce(moveVector * moveSpeed);
+		rigidbody.AddForce(moveVector * moveAcceleration);
 		rigidbody.velocity = Vector2.ClampMagnitude(rigidbody.velocity, moveSpeed);
 	}
 
@@ -61,18 +66,49 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	private void CheckGrounded() {
-		// TODO
+		if (rigidbody.velocity.y < 0) { // TODO ignore when already grounded? need to fix walking off platform
+			Vector2 rayPos = ((Vector2)transform.position) + rayOrigin;
+			int groundedMask = LayerMask.GetMask("Groundable");
+			Debug.DrawRay(rayPos, Vector2.down * rayDist, Color.yellow, 0.1f);
+			List<RaycastHit2D> results = new List<RaycastHit2D>(1);
+			ContactFilter2D cf = new ContactFilter2D();
+			cf.SetLayerMask(groundedMask);
+			if (Physics2D.Raycast(rayPos, Vector2.down, cf, results, rayDist) > 0) {
+				transform.position = results[0].point;
+				SetGrounded(true);
+
+			}
+		}
+	}
+
+	private void SetGrounded(bool grounded) {
+		isGrounded = grounded;
+		if (grounded) {
+			rigidbody.velocity = new Vector2(rigidbody.velocity.x, 0);
+		}
 	}
 
 	private void OnJump(InputValue value) {
-		Vector2 jumpVector = new Vector2(0, jumpForce);
-		rigidbody.AddForce(jumpVector);
+		if (isGrounded) {
+			Vector2 jumpVector = new Vector2(0, jumpForce);
+			rigidbody.AddForce(jumpVector);
+			SetGrounded(false);
+		}
+	}
+
+	private void ToggleGravity() {
+		rigidbody.gravityScale = rigidbody.gravityScale > Mathf.Epsilon ? 0 : 1f;
+	}
+
+	private void ToggleGravity(bool on) {
+		rigidbody.gravityScale = on ? 1f : 0;
 	}
 #endregion
 
 #region CHEATS
 	private void OnFlyToggled() {
-		rigidbody.gravityScale = flyCheat ? 0 : 1;
+		ToggleGravity(!flyCheat);
+		SetGrounded(false);
 	}
 #endregion
 }
