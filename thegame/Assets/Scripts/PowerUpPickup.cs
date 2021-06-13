f8b501ec125255c9c920365ef1a13dec501b2402
DarrenHobern <darren.hobern@gmail.com>
2021-06-14 00:45:15 +1200

    Explode anim + better logic

--- thegame/Assets/Scripts/PowerUpPickup.cs
+++ thegame/Assets/Scripts/PowerUpPickup.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 
 public class PowerUpPickup : LineSegment {
 
+	[SerializeField] private Animator animator;
 	public bool isBomb = true;
-	[ShowIf("isBomb")][SerializeField] private float bombRadius = 2f;
+	[ShowIf("isBomb")][SerializeField] private float bombRadius = 0.5f;
 	public int score = 1;
 
 	private void Awake() {
@@ -17,17 +18,21 @@ public class PowerUpPickup : LineSegment {
 	public void Explode() {
 		// overlapsphere and find the poweruppickups and destroy them
 		// remove score
+		animator.SetTrigger("explode");
 		LayerMask mask = LayerMask.GetMask("Pickup");
 		Collider2D[] pickups = Physics2D.OverlapCircleAll(transform.position, bombRadius, mask, -1f, 1f);
 		foreach (Collider2D p in pickups)
 		{
-			if (p.GetComponent<PowerUpPickup>() != null) {
-				if (p.transform.parent != null) {
-					p.transform.parent.GetComponent<LineSegment>()?.ClearSlot();
+			if (p.gameObject != gameObject) {
+				if (p.GetComponent<PowerUpPickup>() != null) {
+					if (p.transform.parent != null) {
+						p.transform.parent.GetComponent<LineSegment>()?.ClearSlot();
+					}
+					Destroy(p.gameObject);
 				}
-				Destroy(p.gameObject);
 			}
 		}
+		Destroy(gameObject, 2f);
 	}
 
 	public void EnableGathering() {
